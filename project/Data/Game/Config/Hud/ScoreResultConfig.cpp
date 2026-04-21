044b0c224ad23f2ee7bb6c372affb3cba2e5e32e
kase haruto <k023g0052@g.neec.ac.jp>
2026-04-21 10:52:28 +0900

    不要なconfigデータの削除

--- project/Data/Game/Config/Hud/ScoreResultConfig.cpp
+++
@@ -1,9 +0,0 @@
-#include "ScoreResultConfig.h"
-
-ScoreResultConfig::ScoreResultConfig() {
-	AddField("delaySec",     delaySec);
-	AddField("countUpSec",   countUpSec);
-	AddField("countEaseInt", countEaseInt);
-}
-
-CalyxEngine::ParamPath ScoreResultConfig::GetParamPath() const { return {CalyxEngine::ParamDomain::Game,"ScoreResultHud"}; }
