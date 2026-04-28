3442975315b1b0175e78e8626af2388a23647ca5
fujitsukaharuto <146906715+fujitsukaharuto@users.noreply.github.com>
2026-04-28 17:52:28 +0900

    クリアシーンとゲームオーバーシーンの作成

--- project/Game/Scene/Clear/ClearScene.cpp
+++ project/Game/Scene/Clear/ClearScene.cpp
@@ -5,44 +5,43 @@
 /////////////////////////////////////////////////////////////////////////////////////////
 
 // scene
-#include <Engine/Scene/Test/TestScene.h>
+#include <Game/Scene/Clear/ClearScene.h>
 #include "Game/Scene/Details/SceneType.h"
 #include "Game/Scene/Utility/SceneTypeUtil.h"
 #include <Game/DemoPlayer/DemoPlayer.h>
 
 // engine
 #include <Engine/Collision/CollisionManager.h>
-#include <Engine/Foundation/Utility/Func/MyFunc.h>
-#include <Engine/Scene/Serializer/SceneSerializer.h>
-#include <Engine/Scene/Context/SceneContext.h>
 #include <Engine/Foundation/Input/Input.h>
+#include <Engine/Foundation/Utility/Func/MyFunc.h>
+#include <Engine/Scene/Context/SceneContext.h>
+#include <Engine/Scene/Serializer/SceneSerializer.h>
 // lib
 
 /////////////////////////////////////////////////////////////////////////////////////////
 //	コンストラクタ/デストラクタ
 /////////////////////////////////////////////////////////////////////////////////////////
-TestScene::TestScene(){
+ClearScene::ClearScene() {
 	// シーン名を設定
-	BaseScene::SetSceneName("TestScene");
-
+	BaseScene::SetSceneName("ClearScene");
 }
 
 /////////////////////////////////////////////////////////////////////////////////////////
 //	アセットのロード
 /////////////////////////////////////////////////////////////////////////////////////////
-void TestScene::LoadAssets(){}
+void ClearScene::LoadAssets() {}
 
 /////////////////////////////////////////////////////////////////////////////////////////
 //	初期化処理
 /////////////////////////////////////////////////////////////////////////////////////////
-void TestScene::Initialize(){
+void ClearScene::Initialize() {
 	sceneContext_->Initialize();
 
-	sceneContext_->SetSceneName("TestScene");
+	sceneContext_->SetSceneName("ClearScene");
 
 	BaseScene::Initialize();
 
-	SceneSerializer::Load(*sceneContext_, "Resources/Assets/Scenes/test.scene");
+	SceneSerializer::Load(*sceneContext_, "Resources/Assets/Scenes/Clear.scene");
 
 	LoadAssets();
 
@@ -51,50 +50,36 @@ void TestScene::Initialize(){
 	//=========================
 	pauseBg_ = std::make_unique<Sprite>("Textures/uvChecker.dds");
 	pauseBg_->Initialize({0.0f, 0.0f}, {1280.0f, 720.0f});
-	pauseBg_->SetColor({0.0f, 0.0f, 0.0f, 0.5f});
+	pauseBg_->SetColor({1.0f, 1.0f, 0.0f, 1.0f});
 	pauseBg_->Update();
 }
 
 /////////////////////////////////////////////////////////////////////////////////////////
 //	更新処理
 /////////////////////////////////////////////////////////////////////////////////////////
-void TestScene::Update([[maybe_unused]]float dt){
-	// ポーズの切り替え
-	if (CalyxFoundation::Input::TriggerKey(DIK_0)) {
-		isPaused_ = !isPaused_;
-		sceneContext_->SetRuntime(!isPaused_);
-	}
-	if(CalyxFoundation::Input::TriggerKey(DIK_9)) {
-		transitionRequestor_->RequestSceneChange(GameSceneUtil::ToSceneId(SceneType::CLEAR));
-	}
-	if(CalyxFoundation::Input::TriggerKey(DIK_8)) {
-		transitionRequestor_->RequestSceneChange(GameSceneUtil::ToSceneId(SceneType::GAMEOVER));
+void ClearScene::Update([[maybe_unused]] float dt) {
+
+	if(CalyxFoundation::Input::TriggerKey(DIK_7)) {
+		transitionRequestor_->RequestSceneChange(GameSceneUtil::ToSceneId(SceneType::TEST));
 	}
 
-	if (isPaused_) {
-		
-		return;
-	}
 
-	//衝突判定
+	// 衝突判定
 	CollisionManager::GetInstance()->UpdateCollisionAllCollider();
 }
 
-void TestScene::Draw(ID3D12GraphicsCommandList* cmdList, PipelineService* psoService, IRenderTarget*  rt){
+void ClearScene::Draw(ID3D12GraphicsCommandList* cmdList, PipelineService* psoService, IRenderTarget* rt) {
 
 	//========================================================//
 	//	spriteの登録
 	//========================================================//
-	if (isPaused_) {
-		spriteRenderer_->Register(pauseBg_.get());
-	}
-	
-	//シーン上のオブジェクトの描画
-	BaseScene::Draw(cmdList, psoService, rt);
+	spriteRenderer_->Register(pauseBg_.get());
 
+	// シーン上のオブジェクトの描画
+	BaseScene::Draw(cmdList, psoService, rt);
 }
 
-void TestScene::CleanUp(){
+void ClearScene::CleanUp() {
 	// 3Dオブジェクトの描画を終了
 	sceneContext_->GetObjectLibrary()->Clear();
 	CollisionManager::GetInstance()->ClearColliders();
--- project/Game/Scene/Gameover/GameoverScene.cpp
+++ project/Game/Scene/Gameover/GameoverScene.cpp
@@ -5,44 +5,43 @@
 /////////////////////////////////////////////////////////////////////////////////////////
 
 // scene
-#include <Engine/Scene/Test/TestScene.h>
 #include "Game/Scene/Details/SceneType.h"
 #include "Game/Scene/Utility/SceneTypeUtil.h"
 #include <Game/DemoPlayer/DemoPlayer.h>
+#include <Game/Scene/Gameover/GameoverScene.h>
 
 // engine
 #include <Engine/Collision/CollisionManager.h>
-#include <Engine/Foundation/Utility/Func/MyFunc.h>
-#include <Engine/Scene/Serializer/SceneSerializer.h>
-#include <Engine/Scene/Context/SceneContext.h>
 #include <Engine/Foundation/Input/Input.h>
+#include <Engine/Foundation/Utility/Func/MyFunc.h>
+#include <Engine/Scene/Context/SceneContext.h>
+#include <Engine/Scene/Serializer/SceneSerializer.h>
 // lib
 
 /////////////////////////////////////////////////////////////////////////////////////////
 //	コンストラクタ/デストラクタ
 /////////////////////////////////////////////////////////////////////////////////////////
-TestScene::TestScene(){
+GameoverScene::GameoverScene() {
 	// シーン名を設定
-	BaseScene::SetSceneName("TestScene");
-
+	GameoverScene::SetSceneName("GameoverScene");
 }
 
 /////////////////////////////////////////////////////////////////////////////////////////
 //	アセットのロード
 /////////////////////////////////////////////////////////////////////////////////////////
-void TestScene::LoadAssets(){}
+void GameoverScene::LoadAssets() {}
 
 /////////////////////////////////////////////////////////////////////////////////////////
 //	初期化処理
 /////////////////////////////////////////////////////////////////////////////////////////
-void TestScene::Initialize(){
+void GameoverScene::Initialize() {
 	sceneContext_->Initialize();
 
-	sceneContext_->SetSceneName("TestScene");
+	sceneContext_->SetSceneName("GameoverScene");
 
 	BaseScene::Initialize();
 
-	SceneSerializer::Load(*sceneContext_, "Resources/Assets/Scenes/test.scene");
+	SceneSerializer::Load(*sceneContext_, "Resources/Assets/Scenes/Gameover.scene");
 
 	LoadAssets();
 
@@ -51,50 +50,35 @@ void TestScene::Initialize(){
 	//=========================
 	pauseBg_ = std::make_unique<Sprite>("Textures/uvChecker.dds");
 	pauseBg_->Initialize({0.0f, 0.0f}, {1280.0f, 720.0f});
-	pauseBg_->SetColor({0.0f, 0.0f, 0.0f, 0.5f});
+	pauseBg_->SetColor({1.0f, 0.0f, 0.0f, 1.0f});
 	pauseBg_->Update();
 }
 
 /////////////////////////////////////////////////////////////////////////////////////////
 //	更新処理
 /////////////////////////////////////////////////////////////////////////////////////////
-void TestScene::Update([[maybe_unused]]float dt){
-	// ポーズの切り替え
-	if (CalyxFoundation::Input::TriggerKey(DIK_0)) {
-		isPaused_ = !isPaused_;
-		sceneContext_->SetRuntime(!isPaused_);
-	}
-	if(CalyxFoundation::Input::TriggerKey(DIK_9)) {
-		transitionRequestor_->RequestSceneChange(GameSceneUtil::ToSceneId(SceneType::CLEAR));
-	}
-	if(CalyxFoundation::Input::TriggerKey(DIK_8)) {
-		transitionRequestor_->RequestSceneChange(GameSceneUtil::ToSceneId(SceneType::GAMEOVER));
+void GameoverScene::Update([[maybe_unused]] float dt) {
+
+	if(CalyxFoundation::Input::TriggerKey(DIK_7)) {
+		transitionRequestor_->RequestSceneChange(GameSceneUtil::ToSceneId(SceneType::TEST));
 	}
 
-	if (isPaused_) {
-		
-		return;
-	}
-
-	//衝突判定
+	// 衝突判定
 	CollisionManager::GetInstance()->UpdateCollisionAllCollider();
 }
 
-void TestScene::Draw(ID3D12GraphicsCommandList* cmdList, PipelineService* psoService, IRenderTarget*  rt){
+void GameoverScene::Draw(ID3D12GraphicsCommandList* cmdList, PipelineService* psoService, IRenderTarget* rt) {
 
 	//========================================================//
 	//	spriteの登録
 	//========================================================//
-	if (isPaused_) {
-		spriteRenderer_->Register(pauseBg_.get());
-	}
-	
-	//シーン上のオブジェクトの描画
-	BaseScene::Draw(cmdList, psoService, rt);
+	spriteRenderer_->Register(pauseBg_.get());
 
+	// シーン上のオブジェクトの描画
+	BaseScene::Draw(cmdList, psoService, rt);
 }
 
-void TestScene::CleanUp(){
+void GameoverScene::CleanUp() {
 	// 3Dオブジェクトの描画を終了
 	sceneContext_->GetObjectLibrary()->Clear();
 	CollisionManager::GetInstance()->ClearColliders();
