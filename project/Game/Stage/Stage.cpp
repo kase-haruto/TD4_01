f750fa3386305e3b7252acdd66c8c4c08dedfec5
fujitsukaharuto <146906715+fujitsukaharuto@users.noreply.github.com>
2026-05-07 10:43:06 +0900

    HPのUI

--- project/Game/Stage/Stage.cpp
+++ project/Game/Stage/Stage.cpp
@@ -1,8 +1,16 @@
 #include "Stage.h"
+#include <Engine/Application/System/Environment.h>
 
 void Stage::Initialize(float hp, float limitTime) {
 	stageHP_ = hp;
+	maxHP_	 = hp;
 	timer_	 = limitTime;
+
+	hpUI_ = std::make_unique<Sprite>("Textures/white1x1.dds");
+	hpUI_->SetColor({1.0f, 0.0f, 0.0f, 1.0f});
+	hpUI_->SetAnchorPoint({0.0f, 0.5f});
+	hpUI_->SetSize(hpUISize_);
+	hpUI_->SetPosition({static_cast<float>(kWindowWidth) * 0.5f - hpUISize_.x * 0.5f, 50.0f});
 
 	numbersSprite_ = std::make_unique<NumbersSprite>(
 		"Textures/Numbers", ".png");
@@ -18,14 +26,26 @@ void Stage::Update(float dt) {
 		numbersSprite_->SetValue(int(timer_));
 		numbersSprite_->Update();
 	}
+	if(hpUI_) {
+		hpUI_->Update();
+	}
 }
 
 void Stage::Draw(SpriteRenderer* renderer) {
 	if(numbersSprite_) {
 		numbersSprite_->Draw(renderer);
 	}
+	if(hpUI_) {
+		renderer->Register(hpUI_.get());
+	}
 }
 
 void Stage::TakeDamage(float damage) {
 	stageHP_ -= damage;
+	if(stageHP_ < 0.0f) {
+		stageHP_ = 0.0f;
+	}
+
+	float rate = stageHP_ / maxHP_;
+	hpUI_->SetSize({hpUISize_.x * rate, hpUISize_.y});
 }
--- project/Game/Stage/Stage.h
+++ project/Game/Stage/Stage.h
@@ -1,5 +1,6 @@
 #pragma once
 #include <Engine\Objects\2D\NumbersSprite\NumbersSprite.h>
+#include <Engine/Renderer/Sprite/Sprite.h>
 
 class Stage {
 public:
@@ -15,11 +16,12 @@ public:
 	void TakeDamage(float damage);
 
 private:
-
-private:
-
+	std::unique_ptr<Sprite>		   hpUI_;
 	std::unique_ptr<NumbersSprite> numbersSprite_;
-	float stageHP_ = 0.0f;
-	float timer_   = 0.0f;
+	float						   stageHP_ = 0.0f;
+	float						   maxHP_	= 0.0f;
+	float						   timer_	= 0.0f;
+
+	CalyxEngine::Vector2 hpUISize_ = {500.0f, 20.0f};
 
 };
