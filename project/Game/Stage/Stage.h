dc21daa4cbe95de543fe55bb360a8839b6fa71e6
fujitsukaharuto <146906715+fujitsukaharuto@users.noreply.github.com>
2026-05-25 04:22:02 +0900

    Fix

--- project/Game/Stage/Stage.h
+++ project/Game/Stage/Stage.h
@@ -30,7 +30,7 @@ private:
 	bool isGameOver_ = false;
 	bool isClear_	 = false;
 
-	CalyxEngine::Vector2 hpUISize_ = {700.0f, 80.0f};
-	CalyxEngine::Vector2 barSize_   = {500.0f, 20.0f};
+	CalyxEngine::Vector2 hpUISize_ = {700.0f, 180.0f};
+	CalyxEngine::Vector2 barSize_   = {500.0f, 50.0f};
 
 };
